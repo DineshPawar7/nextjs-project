dc1dbfbf0d465d574abca6ced3eec660d57de55b
Dinesh Pawar <dsp70458@gmail.com>
2025-07-17 11:32:57 +0530

    first commit

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,9 @@
+import {z} from "zod"
+
+export const messageSchema = z.object({
+    content: z
+    .string()
+    .min(10, {message: "content must be at least of 10 char"})
+    .max(300, {message: "content must be no longer then 300 char"})
+
+})
